4f479db49e6b212133d051d5a5dbf69ed6ada220
Jasmine Kay <jasminekay505@gmail.com>
2021-05-13 14:38:36 +0800

    attempting basic search bar and button

---
+++ src/components/Search/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function Search(props) { 
+    return ( 
+        <form className="search">
+      <div className="form-group">
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          type="search"
+          className="form-control"
+          placeholder="Search for a User"
+          id="searchInput"
+        />
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+          Search
+        </button>
+      </div>
+    </form>
+    )
+}
+
+export default Search;
