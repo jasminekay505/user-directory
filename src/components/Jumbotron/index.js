6957bfa229d46ac39bd6587682640d3994fa61c3
Jasmine Kay <jasminekay505@gmail.com>
2021-05-13 14:26:54 +0800

    Setting up basic jumbotron

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">User Directory</h1>
+                <p class="lead">Filter for users by clicking the column or search for a specific user using the search bar.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
